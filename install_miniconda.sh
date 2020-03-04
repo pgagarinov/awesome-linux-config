5bf2f6aff156717c559b551590952c479fcbac0c
Peter Gagarinov <pgagarinov@gmail.com>
2020-03-05 01:13:06 +0300

    fixes

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -3,6 +3,7 @@ set -e
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo adduser miniconda
 sudo sh ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
+rm ./Miniconda3-latest-Linux-x86_64.sh
 sudo chown -R miniconda:miniconda /opt/miniconda
 sudo chmod -R go-w /opt/miniconda 
 sudo chmod -R go+rX /opt/miniconda 
--- install_rust_utils.sh
+++ install_rust_utils.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+cargo install -j$(nproc) broot
+broot --install
+cargo install -j$(nproc) bandwhich
+cargo install -j$(nproc) fd-find
+cargo install -j$(nproc) du-dust
