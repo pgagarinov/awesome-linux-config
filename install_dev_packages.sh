85889abd63f4ff046edb96282437b9e7e5934a7a
Peter Gagarinov <pgagarinov@gmail.com>
2020-03-05 00:20:02 +0300

    fixes

--- install_dev_packages.sh
+++ install_dev_packages.sh
@@ -4,19 +4,4 @@ echo 'set bell-style none' >> ~/.inputrc
 sudo yum install epel-release -y
 sudo yum install wget -y
 sudo yum install libzstd -y
-sudo yum install git git-lfs tig htop mc nano screen ncdu tigervnc-server -y
-#install git-lfs
-#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
-#sudo yum install git-lfs -y
-#git lfs install
-#sudo yum install make ncurses-devel ncurses -y
-#sudo yum groupinstall 'Development Tools'
-#https://github.com/jonas/tig/releases/download/tig-2.5.0/tig-2.5.0.tar.gz
-#tar -xvf ./tig-2.5.0.tar.gz
-#cd ./tig-2.5.0/
-#make
-#make install
-# Doesn't yet work on CentOS 8
-# sudo yum install sysbench go
-# go get github.com/prasmussen/gdrive
-# sysbench --test=cpu --cpu-max-prime=20000 run
+sudo yum install tig htop mc nano screen ncdu tigervnc-server -y
---
+++ install_setup_git.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+sudo dnf install -y git git-lfs
+git config --global user.name "Peter Gagarinov"
+git config --global user.email "pgagarinov@gmail.com"
