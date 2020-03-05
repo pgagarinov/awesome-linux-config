2fa930e5078d9fa2bd381cfe19a071fbf01de5e6
Peter Gagarinov <pgagarinov@gmail.com>
2020-03-05 16:14:26 +0300

    changes

--- install_rust_utils.sh
+++ install_rust_utils.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -e
 
-rm -rf ./.cargo
-rm -rf ./.config/broot
-rm -rf ./.config/pulse
+#rm -rf ./.cargo
+#rm -rf ./.config/broot
+#rm -rf ./.config/pulse
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 cargo install -j$(nproc) broot
 broot --install
-cargo install -j$(nproc) bandwhich
+#cargo install -j$(nproc) bandwhich
 cargo install -j$(nproc) fd-find
 cargo install -j$(nproc) du-dust
 
