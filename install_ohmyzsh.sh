69f44736f89a795c08e2b406eb1dc14bd80f8443
Peter Gagarinov <pgagarinov@gmail.com>
2020-03-05 16:34:09 +0300

    fixes

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -19,7 +19,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-#cp ./config/.zshrc ~
-#cp ./config/.p10k.zsh ~
+cp ./config/.zshrc ~
+cp ./config/.p10k.zsh ~
 
 
