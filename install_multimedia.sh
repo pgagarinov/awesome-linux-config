2daf1a47040d245bd8d76fdb8f7f70a796c3df2f
Peter Gagarinov <pgagarinov@gmail.com>
2020-03-12 14:31:06 +0300

    fixes

---
+++ install_multimedia.sh
@@ -0,0 +1,7 @@
+sudo dnf localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
+sudo dnf install --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
+sudo dnf install http://rpmfind.net/linux/epel/7/x86_64/Packages/s/SDL2-2.0.10-1.el7.x86_64.rpm
+sudo dnf install ffmpeg
+sudo dnf -y install ffmpeg-devel
+sudo dnf -y install vlc
+
