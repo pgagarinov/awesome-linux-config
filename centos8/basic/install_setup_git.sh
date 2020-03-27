a05308e7a1899cea78600bff371bda99fc520baa
Peter Gagarinov <pgagarinov@gmail.com>
2020-03-27 10:00:55 +0300

    commented out the template for both user name and email git settings in install_setup_git.sh, track x bit in all git repos by default

--- centos8/basic/install_setup_git.sh
+++ centos8/basic/install_setup_git.sh
@@ -3,3 +3,10 @@ set -e
 sudo dnf install -y git git-lfs
 #git config --global user.name "Your Name"
 #git config --global user.email "your@email"
+
+# https://stackoverflow.com/questions/1580596/how-do-i-make-git-ignore-file-mode-chmod-changes
+# https://medium.com/@tahteche/how-git-treats-changes-in-file-permissions-f71874ca239d
+
+# we do not plan to use Windows so we decide to track execute bits to avoid calling 
+# git update-index --chmod=+x ./file.sh everytime we need to track x bit
+git config --global core.fileMode true
