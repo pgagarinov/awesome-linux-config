61a10de759cf1c34e2d64462155119967c41e9c3
Peter Gagarinov <pgagarinov@gmail.com>
2020-04-05 17:17:54 +0300

    fixes for Selenoid

---
+++ centos8/selenoid/README.md
@@ -0,0 +1,1 @@
+Please make sure to change your IPs in selenoid_quotes/quotas_all.xml
--- centos8/selenoid/start_selenoid_head.sh
+++ centos8/selenoid/start_selenoid_head.sh
@@ -3,15 +3,16 @@ set -e
 LOCAL_IP=`hostname -I | awk '{print $1}'`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #
+
+docker stop ggr||true&&docker rm ggr||true
+docker stop ggr-ui||true&&docker rm ggr-ui||true
+
 ./cm selenoid stop
 ./cm selenoid cleanup
 ./cm selenoid-ui stop
 ./cm selenoid-ui cleanup
-docker stop ggr
-docker rm ggr
-docker stop ggr-ui
-docker rm ggr-ui
+
 ./cm selenoid start --browsers "chrome:75" --last-versions 2 --tmpfs 512 --vnc --port 4445 --args "-limit 24"
-docker run -d --name ggr -p 4444:4444 -v ${SCRIPT_DIR}/selenoid_quotas:/etc/grid-router/quota:ro aerokube/ggr:latest$
-docker run -d --name ggr-ui -p 8888:8888 -v ${SCRIPT_DIR}/selenoid_quotas:/etc/grid-router/quota:ro aerokube/ggr-ui:$
+docker run -d --name ggr -p 4444:4444 -v ${SCRIPT_DIR}/selenoid_quotas:/etc/grid-router/quota:ro aerokube/ggr:latest-release
+docker run -d --name ggr-ui -p 8888:8888 -v ${SCRIPT_DIR}/selenoid_quotas:/etc/grid-router/quota:ro aerokube/ggr-ui:latest-release
 ./cm selenoid-ui start --args "--period 1000ms --selenoid-uri=http://${LOCAL_IP}:8888" --port 8080
