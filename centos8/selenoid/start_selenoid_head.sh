c64871953e710696a33e47285f6606348893a57f
Peter Gagarinov <pgagarinov@gmail.com>
2020-04-06 12:17:02 +0300

    selenoid tmpfs adjustments

--- centos8/selenoid/start_selenoid_head.sh
+++ centos8/selenoid/start_selenoid_head.sh
@@ -3,6 +3,7 @@ set -e
 LOCAL_IP=`hostname -I | awk '{print $1}'`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #
+N_MAX_SESSIONS=24
 
 docker stop ggr||true&&docker rm ggr||true
 docker stop ggr-ui||true&&docker rm ggr-ui||true
@@ -12,7 +13,8 @@ docker stop ggr-ui||true&&docker rm ggr-ui||true
 ./cm selenoid-ui stop
 ./cm selenoid-ui cleanup
 
-./cm selenoid start --browsers "chrome:75" --last-versions 2 --tmpfs 512 --vnc --port 4445 --args "-limit 24"
+./cm selenoid start --browsers "chrome:75" --last-versions 2 --tmpfs 1024 --vnc --port 4445 --args "-limit $N_MAX_SESSIONS"
+
 docker run -d --name ggr -p 4444:4444 -v ${SCRIPT_DIR}/ggr_data:/etc/grid-router/:ro aerokube/ggr:latest-release -guests-allowed -guests-quota quota_all
 docker run -d --name ggr-ui -p 8888:8888 -v ${SCRIPT_DIR}/ggr_data:/etc/grid-router/:ro aerokube/ggr-ui:latest-release 
 ./cm selenoid-ui start --args "--period 1000ms --selenoid-uri=http://${LOCAL_IP}:8888" --port 8080
--- centos8/selenoid/start_selenoid_node.sh
+++ centos8/selenoid/start_selenoid_node.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # specify number of browser sessions as input parameter
 set -e
+N_MAX_SESSIONS=24
 ./cm selenoid stop
 ./cm selenoid cleanup
 ./cm selenoid-ui stop
 ./cm selenoid-ui cleanup
-./cm selenoid start --browsers "chrome:75" --last-versions 2 --tmpfs 512 --vnc --port 4444 --args "-limit $1"
+./cm selenoid start --browsers "chrome:75" --last-versions 2 --tmpfs 1024 --vnc --port 4444 --args "-limit $N_MAX_SESSIONS"
 ./cm selenoid-ui start --args "--period 1000ms" --port 8080
 
