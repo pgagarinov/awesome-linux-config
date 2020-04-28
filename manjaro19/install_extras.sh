d999baaeff4c767d3f357f14a21fafd614395e30
Peter Gagarinov <pgagarinov@gmail.com>
2020-04-28 14:57:07 +0300

    added keepass

--- manjaro19/install_extras.sh
+++ manjaro19/install_extras.sh
@@ -87,3 +87,6 @@ sudo pacman -S --noconfirm virt-manager qemu vde2 ebtables dnsmasq bridge-utils 
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 
+# Keepass password manager
+sudo pacman -S --noconfirm keepass
+yay -S --noconfirm keepass-plugin-http
