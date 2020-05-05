8c545298227a6fb078824ea5bbb87068a02f340e
Valentin Suruceanu <valentin.suruceanu@gmail.com>
2020-05-05 22:28:11 +0300

    Changed logic of remove_canonical_stuff.sh

--- manjaro19/basic/remove_canonical_stuff.sh
+++ manjaro19/basic/remove_canonical_stuff.sh
@@ -2,10 +2,10 @@
 set -e
 if command -v snap;
 then
-	printf "${MSGCOLOR}Canonical stuff allready removed${NOCOLOR}\n"
-else
 	# We do not want Canonical stuff)
 	sudo systemctl stop snapd.socket
 	sudo systemctl disable snapd.socket
 	sudo pacman -Rns --noconfirm snapd
+else
+	printf "${MSGCOLOR}Canonical stuff allready removed${NOCOLOR}\n"
 fi
