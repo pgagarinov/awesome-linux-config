f351d1e42bbbeab47a5b8db892c1703c42fbd491
Peter Gagarinov <pgagarinov@gmail.com>
2020-05-17 02:25:19 +0300

    made it possible to run minikube start right after docker install as part of the same parent script

--- manjaro19/basic/install_minikube.sh
+++ manjaro19/basic/install_minikube.sh
@@ -2,6 +2,16 @@
 set -e
 sudo pacman -S --noconfirm minikube
 sudo pacman -S --noconfirm kubectl
+
+## Check that minikube works as expected
+
+# first become another group, this is the same as 'newgrp docker' but inside the same shell
+group=docker
+if [ $(id -gn) != $group ]; then
+ exec sg $group "$0 $*"
+fi
+
 minikube start
 minikube stop
-#type `minikube dashboard` or `minikube dashboard --url` to start Kubernetes dashboard
+
+## type `minikube dashboard` or `minikube dashboard --url` to start Kubernetes dashboard
