c54e67ebd65bb50cc588a15bbdb54570482724b9
Peter Gagarinov <pgagarinov@gmail.com>
2020-05-20 17:10:57 +0300

    added rsync for Arch

--- manjaro19/basic/install_dev_packages.sh
+++ manjaro19/basic/install_dev_packages.sh
@@ -5,5 +5,5 @@ sudo pacman -S --noconfirm htop neofetch most
 sudo pacman -S --noconfirm screen
 sudo pacman -S --noconfirm nano vim nano-syntax-highlighting
 sudo pacman -S --noconfirm mc ncdu
-sudo pacman -S --noconfirm wget curl bind-tools
+sudo pacman -S --noconfirm wget curl bind-tools rsync
 echo "include /usr/share/nano-syntax-highlighting/*.nanorc" >> ~/.nanorc
