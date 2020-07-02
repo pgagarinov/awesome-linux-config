a4ad124f11ee8f84d5e796d16b5b8bf230c4d66e
Peter Gagarinov <pgagarinov@gmail.com>
2020-07-02 14:57:36 +0300

    changed something

--- centos8/install_nvidia_driver2.sh
+++ centos8/install_nvidia_driver2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 #sudo sh ./NVIDIA-Linux-x86_64-440.64.run
-#rm ./NVIDIA-Linux-x86_64-440.64.run
+rm ./NVIDIA-Linux-x86_64-440.64.run
 sudo sh ./NVIDIA-Linux-x86_64-435.21.run
 rm ./NVIDIA-Linux-x86_64-435.21.run
 sudo systemctl set-default graphical.target
