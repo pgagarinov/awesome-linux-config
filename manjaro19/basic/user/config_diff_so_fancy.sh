f2e155842b379a3309c63bf4c63bb7ab2aaccdd3
Valentin Suruceanu <valentin.suruceanu@gmail.com>
2020-07-22 15:45:42 +0300

    updated diff so fancy script

--- manjaro19/basic/user/config_diff_so_fancy.sh
+++ manjaro19/basic/user/config_diff_so_fancy.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 set -e
 
-broot --install
+#broot --install
 
 # Configure git to use d-s-f for *all* diff operations
+echo "config diff so fancy paget in git"
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
+echo "check if binary is exported, if no add this config to zprofile"
 grep -qxF 'export PATH="$HOME/.cargo/bin:$PATH"' ~/.zprofile || echo 'export PATH="$HOME/.cargo/bin:$PATH"'>> ~/.zprofile
-source ~/.zshrc
+#echo "update zshrc"
+#source ~/.zshrc
+echo "update zprofile"
 source ~/.zprofile
