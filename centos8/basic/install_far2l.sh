a787a05ca989f76b84bd462aadaf6205cc4dbd3e
Peter Gagarinov <pgagarinov@gmail.com>
2020-08-01 16:04:23 +0300

    Added missing depedencies for FAR2l on CentOS 8

--- centos8/basic/install_far2l.sh
+++ centos8/basic/install_far2l.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
+
+sudo dnf -y install gawk m4 glib2-devel wxGTK3-devel cmake gcc-c++ git
+
+
 sudo dnf -y install dnf-plugins-core
 sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf config-manager --set-enabled PowerTools
+
 
 sudo dnf -y install libssh-devel
 sudo dnf -y install libsmbclient-devel
@@ -10,6 +15,8 @@ sudo dnf -y install libnfs-devel
 sudo dnf -y install libarchive-devel
 sudo dnf -y install python3-devel
 sudo dnf -y install virtualenv
+sudo dnf -y install openssl-devel
+sudo dnf -y install neon-devel
 
 rm -rf ./far2l
 git clone https://github.com/elfmz/far2l
@@ -20,3 +27,4 @@ cmake3 -DUSEWX=no -DPYTHON=yes -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 sudo make install
 cd ../..
+rm -rf ./far2l
