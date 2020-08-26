79306e4542ea4cb4cc8cbf4ebb03c041e1891abe
Peter Gagarinov <pgagarinov@gmail.com>
2020-08-26 12:21:32 +0300

    Added support for Russian language spellchecking in KDE apps

--- manjaro19/basic/install_spellchecking.sh
+++ manjaro19/basic/install_spellchecking.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -e
-sudo pacman -S --noconfirm --needed aspell-en libmythes mythes-en languagetool
+sudo pacman -S --noconfirm --needed aspell-en libmythes mythes-en languagetool aspell-ru 
+yay -S --noconfirm --needed hunspell-ru
