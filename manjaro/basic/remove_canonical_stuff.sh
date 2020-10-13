47cf54ce02db8b70e8e05c68f4a08f0ea11054db
Peter Gagarinov <pgagarinov@gmail.com>
2020-10-13 12:30:21 +0300

    Added 'c' (for cascade) flag  for snapd removal to make sure all its dependencies are also removed (on Manjaro)

--- manjaro/basic/remove_canonical_stuff.sh
+++ manjaro/basic/remove_canonical_stuff.sh
@@ -5,7 +5,7 @@ then
 	# We do not want Canonical stuff)
 	sudo systemctl stop snapd.socket
 	sudo systemctl disable snapd.socket
-	sudo pacman -Rns --noconfirm snapd
+	sudo pacman -Rnsc --noconfirm snapd
 else
 	printf "${MSGCOLOR}Canonical stuff allready removed${NOCOLOR}\n"
 fi
