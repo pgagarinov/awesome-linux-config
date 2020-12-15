bea876fe6c081984f3e5e327c2f97652e52fbcc3
Peter Gagarinov <pgagarinov@users.noreply.github.com>
2020-12-15 13:47:19 +0300

    Update install_allure_user.sh
    Removed confusing "update zshrc" messaged

--- centos8/basic/install_allure_user.sh
+++ centos8/basic/install_allure_user.sh
@@ -10,5 +10,4 @@ tar -zxf "allure-commandline-${ALLURE_VERSION}.tgz" -C ~/.allure && rm -f "allur
 ln -s "$HOME/.allure/allure-${ALLURE_VERSION}/bin/allure" ~/.allure/allure  
 
 grep -qxF 'export PATH="$HOME/.allure:$PATH"' ~/.zshrc || sed -i '1s/^/export PATH="$HOME\/.allure:$PATH"\n/' ~/.zshrc
-echo "update zshrc"
 source ~/.zshrc || true
