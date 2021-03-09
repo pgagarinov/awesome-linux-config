eb09e0ebe25131260ea52faa615bccd128e60560
Peter Gagarinov <pgagarinov@gmail.com>
2021-03-09 16:36:41 +0300

    Rust stable chain is now initialized before attempting to install dust from AUR

--- manjaro/basic/install_rust_utils.sh
+++ manjaro/basic/install_rust_utils.sh
@@ -3,6 +3,9 @@ set -e
 
 sudo pacman -S --noconfirm --needed llvm clang
 sudo pacman -S --noconfirm --needed rustup
+
+rustup default stable
+
 sudo pacman -S --noconfirm --needed broot bandwhich fd bat diff-so-fancy exa skim
 broot --install
-yay -S --noconfirm --needed dust
+yay -S --noconfirm --needed --nocleanmenu --nodiffmenu dust
