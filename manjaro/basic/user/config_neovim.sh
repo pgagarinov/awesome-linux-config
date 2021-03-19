678760732d90fcfc341bb5d83a244fc8509b2b9d
Peter Gagarinov <pgagarinov@gmail.com>
2021-03-19 10:08:39 +0300

    FIX: coc-rls NeoVim's plugin for Rust language integration is not installed

--- manjaro/basic/user/config_neovim.sh
+++ manjaro/basic/user/config_neovim.sh
@@ -4,4 +4,5 @@ rm -rf ~/.config/nvim
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 cp -r $dir_path/config/.config/nvim ~/.config
-nvim --headless  +PlugInstall +"CocInstall coc-rls" +qa
+nvim --headless  +PlugInstall +qa
+nvim --headless  +"CocInstall coc-rls" +qa
