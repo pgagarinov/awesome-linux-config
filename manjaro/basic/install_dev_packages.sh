5c6020328f9605d0bffdd04a03089a9f8410de3c
Peter Gagarinov <pgagarinov@gmail.com>
2021-03-23 16:43:53 +0300

    ADD: p7zip for Manjaro/Arch Linux

--- manjaro/basic/install_dev_packages.sh
+++ manjaro/basic/install_dev_packages.sh
@@ -6,5 +6,5 @@ sudo pacman -S --noconfirm --needed screen tmux
 sudo pacman -S --noconfirm --needed nano vim nano-syntax-highlighting neovim
 sudo pacman -S --noconfirm --needed mc ncdu
 sudo pacman -S --noconfirm --needed wget curl bind-tools rsync
-sudo pacman -S --noconfirm --needed unzip
+sudo pacman -S --noconfirm --needed unzip p7zip
 yay -S --noconfirm --needed --nocleanmenu --nodiffmenu neovim-plug
