570437cc218a23613c10680a08917500dc5b7eb7
Peter Gagarinov <pgagarinov@gmail.com>
2021-03-27 19:00:00 +0300

    FIX: miniconda user is not created by ./install_miniconda.sh inside docker container

--- manjaro/basic/install_miniconda.sh
+++ manjaro/basic/install_miniconda.sh
@@ -3,7 +3,7 @@ set -e
 sudo rm -rf /opt/miniconda
 sudo rm -rf ~/.conda
 wget -P ~ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-id -u miniconda &>/dev/null||sudo useradd miniconda
+sudo useradd miniconda||true
 sudo sh ~/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
 sudo usermod -d /opt/miniconda miniconda
 rm -rf ~/Miniconda3-latest-Linux-x86_64.sh*
