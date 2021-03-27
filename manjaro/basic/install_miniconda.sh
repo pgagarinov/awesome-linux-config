740f0ce24614cc9740a6ce92edb9de8e0398fdf8
Peter Gagarinov <pgagarinov@gmail.com>
2021-03-27 18:45:43 +0300

    CHANGE: $HOME is replaced with ~

--- manjaro/basic/install_miniconda.sh
+++ manjaro/basic/install_miniconda.sh
@@ -2,12 +2,11 @@
 set -e
 sudo rm -rf /opt/miniconda
 sudo rm -rf ~/.conda
-wget -P $HOME https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget -P ~ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 id -u miniconda &>/dev/null||sudo useradd miniconda
-
-sudo sh $HOME/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
+sudo sh ~/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
 sudo usermod -d /opt/miniconda miniconda
-rm -rf $HOME/Miniconda3-latest-Linux-x86_64.sh*
+rm -rf ~/Miniconda3-latest-Linux-x86_64.sh*
 sudo chown -R miniconda:miniconda /opt/miniconda
 sudo chmod -R go-w /opt/miniconda 
 sudo chmod -R go+rX /opt/miniconda 
