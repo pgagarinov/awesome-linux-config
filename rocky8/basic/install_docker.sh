424ca5e20edcf4bb5e5ac7b767fb54f06e7a4d62
Peter Gagarinov <pgagarinov@gmail.com>
2021-10-16 22:34:09 +0300

    centos8 scripts now have Rocky Linux 8 as a target

--- rocky8/basic/install_docker.sh
+++ rocky8/basic/install_docker.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 sudo dnf remove -y podman-manpages
-sudo dnf install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install -y docker-ce
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
 
 set -e
 if [ -x "$(command -v firewall-cmd)" ];
