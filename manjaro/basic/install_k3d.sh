87400f5c8c488e200f3910410837cd7662d1e789
Peter Gagarinov <pgagarinov@fastmail.com>
2022-09-28 20:00:12 +0300

    added installation of ns and ctx plugins for kubectl

--- manjaro/basic/install_k3d.sh
+++ manjaro/basic/install_k3d.sh
@@ -3,7 +3,7 @@ set -e
 sudo pacman -S --noconfirm --needed kubectl
 yay -S --noconfirm --needed krew-bin
 yay -S --noconfirm --needed rancher-k3d-bin
-
+kubectl krew install ns ctx
 ## Check that minikube works as expected
 
 # first become another group, this is the same as 'newgrp docker' but inside the same shell
