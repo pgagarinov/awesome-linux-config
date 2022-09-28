c9f249480d95596d50b43204f5f26d6d4e04f084
Peter Gagarinov <pgagarinov@users.noreply.github.com>
2022-09-28 16:20:33 +0200

    Update select_mirrors_and_update.sh

--- manjaro/basic/select_mirrors_and_update.sh
+++ manjaro/basic/select_mirrors_and_update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# reset if necessary
-# sudo pacman-mirrors --country all --api --protocols all --set-branch stable && sudo pacman --noconfirm -Syyu
-sudo pacman-mirrors --fasttrack && sudo pacman --noconfirm -Syyuu
+sudo pacman -S --noconfirm pacman-mirrors 
+sudo pacman-mirrors --geoip
+
 
