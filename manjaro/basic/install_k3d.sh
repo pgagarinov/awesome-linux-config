c947296932fa65c9f2f1345ae282d1b7f2d9a1b7
Peter Gagarinov <pgagarinov@users.noreply.github.com>
2022-10-12 16:00:37 +0200

    Update install_k3d.sh
    +k9s terminal-based dashboard for Kubernetes

--- manjaro/basic/install_k3d.sh
+++ manjaro/basic/install_k3d.sh
@@ -3,6 +3,7 @@ set -e
 sudo pacman -S --noconfirm --needed kubectl
 yay -S --noconfirm --needed krew-bin
 yay -S --noconfirm --needed rancher-k3d-bin
+yay -S --noconfirm --needed k9s
 kubectl krew install ns ctx
 ## Check that minikube works as expected
 
