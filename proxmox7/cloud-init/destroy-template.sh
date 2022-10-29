1e7a1d6eb7d7b64134b8ee9a79ac892dd99bb5b0
Peter Gagarinov <pgagarinov@gmail.com>
2022-10-29 16:52:57 +0300

    Make destroy-template verify that ID is not empty

--- proxmox7/cloud-init/destroy-template.sh
+++ proxmox7/cloud-init/destroy-template.sh
@@ -9,6 +9,12 @@ set -e
 MAIN_MSGCOLOR=`tput setaf 48`
 MSGCOLOR=`tput setaf 3`
 NOCOLOR=`tput sgr0`
+ERRCOLOR=`tput setaf 196`
+
+if [ -z ${Pz_VM_TEMPLATE_ID} ]; then
+   printf "${ERRCOLOR}Pz_VM_TEMPLATE_ID is not set\n${NOCOLOR}"
+   exit 1
+fi
 
 main_msg="${MAIN_MSGCOLOR}========Destroying VM Template with ID=${Pz_VM_TEMPLATE_ID}${NOCOLOR}"
 printf "$main_msg...\n"
