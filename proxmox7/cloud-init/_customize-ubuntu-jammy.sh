0c67dc5eed48eb6cc597f71719d1b4607c99134b
Peter Gagarinov <pgagarinov@gmail.com>
2022-11-13 14:48:14 +0300

    Cleaner config, improved customization of Ubuntu Jammy via selection of fastest mirrors from RU and GE.

--- proxmox7/cloud-init/_customize-ubuntu-jammy.sh
+++ proxmox7/cloud-init/_customize-ubuntu-jammy.sh
@@ -12,9 +12,19 @@ printf "$msg...\n"
 virt-customize -a $1 --install nala,qemu-guest-agent
 printf "$msg: done\n"
 
+msg="===Choosing the fastest mirrors"
+printf "$msg...\n"
+virt-customize -a $1 --run-command 'mkdir /run/lock; printf "y\n"|nala fetch --fetches 3 -c RU -c GE --auto'
+printf "$msg: done\n"
+
 msg="===Removing Snap"
 printf "$msg...\n"
 virt-customize -a $1 --run-command 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/Alliedium/remove-snap/main/remove-snap.sh)"'
+printf "$msg: done\n"
+
+msg="===Cleaning via Nala"
+printf "$msg...\n"
+virt-customize -a $1 --run-command 'nala clean'
 printf "$msg: done\n"
 
 
