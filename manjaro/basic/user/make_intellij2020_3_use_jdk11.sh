dcbc8333a420676427c3fddc8495d8ec714024a1
Anatoli D <a.drumi@yandex.ru>
2022-12-09 16:39:32 +0200

    Update make_intellij2020_3_use_jdk11.sh
    change version IDEA

--- manjaro/basic/user/make_intellij2020_3_use_jdk11.sh
+++ manjaro/basic/user/make_intellij2020_3_use_jdk11.sh
@@ -2,5 +2,7 @@
 set -e
 mkdir -p ~/.config/JetBrains/IntelliJIdea2020.3
 mkdir -p ~/.config/JetBrains/IntelliJIdea2021.1
+mkdir -p ~/.config/JetBrains/IntelliJIdea2021.2
 echo '/usr/lib/jvm/java-11-openjdk/' > ~/.config/JetBrains/IntelliJIdea2020.3/idea.jdk
 echo '/usr/lib/jvm/java-11-openjdk/' > ~/.config/JetBrains/IntelliJIdea2021.1/idea.jdk
+echo '/usr/lib/jvm/java-11-openjdk/' > ~/.config/JetBrains/IntelliJIdea2021.2/idea.jdk
