dc6d51da06b608fa906f431250d1a560d598d71f
Peter Gagarinov <pgagarinov@users.noreply.github.com>
2023-03-24 22:53:21 +0100

    add w3m

--- manjaro/basic/install_server_extras.sh
+++ manjaro/basic/install_server_extras.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 set -e
+
+# w3m text browser
+yay -S --noconfirm --needed w3m
+
 # Stack Haskell platform (dependency for ERD)
-yay -S --noconfirm --needed stack
+yay -S --noconfirm --needed stack 
 
 # FAR for Linux
 # disabled for now as this AUR package is broken
