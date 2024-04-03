9173162b284a9542775fb2a0e78c7991352887b3
Peter Gagarinov <pgagarinov@users.noreply.github.com>
2024-04-03 14:27:05 +0200

    Update install_extras_user.sh

--- manjaro/basic/user/install_extras_user.sh
+++ manjaro/basic/user/install_extras_user.sh
@@ -5,7 +5,7 @@ flatpak remote-add --user flathub https://flathub.org/repo/flathub.flatpakrepo -
 flatpak install -y --user flathub com.viber.Viber
 
 # Libre Office
-flatpak install -y --user flathub org.libreoffice.LibreOffice
+sudo pacman -S libreoffice-fresh --noconfirm
 
 #Install EasySSH
 flatpak install -y --user flathub com.github.muriloventuroso.easyssh
